1da1d5ffa550fa41e20e42ae9be55b3f78fcbd6c
umigit <hamu56Kiki>
2019-03-13 19:48:34 +0900

    change file

--- db/migrate/20190313071728_add_user_ref_to_addresses.rb
+++ db/migrate/20190313071728_add_user_ref_to_addresses.rb
@@ -1,6 +1,5 @@
 class AddUserRefToAddresses < ActiveRecord::Migration[5.2]
   def change
     add_reference :addresses, :user, foreign_key: true
-    change_column_null :addresses, :user_id, false
   end
 end
