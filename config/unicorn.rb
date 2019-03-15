35087a79f29d9b9a5971825b32ea9faeebc9661f
Kohei1918 <ess.hands.2016@gmail.com>
2019-03-15 17:16:52 +0900

    Unicorn修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,13 @@
 app_path = File.expand_path('../../../', __FILE__)
 
-
 worker_processes 1
 
-working_directory app_path
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
 
 
 listen 3000
