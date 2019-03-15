3c2c02cdba1e851abddbe7da186a84063b5d2d12
Kohei1918 <ess.hands.2016@gmail.com>
2019-03-15 13:56:08 +0900

    rbenv_ruby記述変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url,  'git@github.com:Kohei1918/freemarket_sample_46a.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/teamA.pem']
