2e6305bcfd6a8d4a80f884fe9bb55e6d3fd138b8
takimotoRyoichi <cc.c2.npr@gmail.com>
2019-03-15 17:03:42 +0900

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
 
-  def logout
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
   resources :items, only: [:new, :create]
-  get  'users' => 'users#logout'
+  resources :users, only: [:index]
 end
