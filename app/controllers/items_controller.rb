60cdd64d90df190af8adcd4332346b8849e11cef
umigit <hamu56Kiki>
2019-03-15 13:29:54 +0900

    edit items new action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
   def new
+    @categories = Category.ransack(parent_id_null: true).result
   end
 
   def create
+  end
+
+  def category
+
   end
 end
