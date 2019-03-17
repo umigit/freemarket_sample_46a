a7b39bc26af73a53b87ab17b018a8d0213a32b5a
umigit <hamu56Kiki>
2019-03-17 19:58:12 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_104238) do
+ActiveRecord::Schema.define(version: 2019_03_17_072703) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_03_13_104238) do
     t.integer "price", null: false
     t.integer "condition", null: false
     t.boolean "sales_condition", default: false, null: false
+    t.string "images"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["region_id"], name: "index_items_on_region_id"
     t.index ["user_id"], name: "index_items_on_user_id"
