7870869235b8e27ebcb38cda8269d907002dd322
umigit <hamu56Kiki>
2019-03-18 17:26:13 +0900

    add redirect

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
 
   def category
