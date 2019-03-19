d9c52f415f53b6c976c07fe8b33c7412597388ec
umigit <hamu56Kiki>
2019-03-19 17:46:06 +0900

    edit s3 setting

--- app/assets/config/initializers/carrierwave.rb
+++ app/assets/config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'freemarket46a'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket46a'
+  config.fog_directory  = 'freemarket-46a'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-46a'
 end
