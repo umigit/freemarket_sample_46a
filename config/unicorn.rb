f1991839937a6b464e76e17e696bd76a01efe13a
umigit <hamu56Kiki>
2019-03-19 17:16:10 +0900

    fix path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,10 +4,10 @@ ENV['BUNDLE_GEMFILE'] = app_path + "/Gemfile"
 worker_processes 1
 
 working_directory app_path
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+pid "#{app_path}/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 
 
