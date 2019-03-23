3dd75128b6cb4e7ff5aa9b34eb1b4a6394756847
umigit <hamu56Kiki>
2019-03-23 20:41:34 +0900

    add index to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_231953) do
+ActiveRecord::Schema.define(version: 2019_03_23_114027) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_231953) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.integer "parent_id"
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
