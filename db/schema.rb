2456a74278de61d3e176cf973735d12d99902440
umigit <hamu56Kiki>
2019-03-24 11:26:06 +0900

    bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_03_23_114027) do
-=======
-ActiveRecord::Schema.define(version: 2019_03_20_231953) do
->>>>>>> Stashed changes
+
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
