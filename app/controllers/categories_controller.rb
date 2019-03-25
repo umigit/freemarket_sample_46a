f49ae75a7e699ab37a33540bfe86b93d0949b688
umigit <hamu56Kiki>
2019-03-25 20:05:53 +0900

    edit view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :get_categories
+
   def show
     @category = Category.find(params[:id])
     @items = Item.ransack(by_category_id: params[:id].to_f).result
