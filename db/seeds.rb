e02b4bd5412264a930e83ceacdbf9446342cc041
umigit <hamu56Kiki>
2019-03-28 15:47:40 +0900

    create user_profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,13 +20,13 @@ if Rails.env.development?
   end
 elsif Rails.env.test?
 else
-  User.create(email: "test@gmail.com", password: "11111111")
+  # User.create(email: "test@gmail.com", password: "11111111")
+  UserProfile.create(last_name: "太郎", first_name: "手巣斗", last_name_kana: "タロウ", first_name_kana: "テスト", birth_y: 2000, birth_m: 1, birth_d: 1, nickname: "てっちゃん", user_id: 1)
+  # CSV.foreach('db/categories.csv', headers: true) do |row|
+  #   Category.create(id: row['id'], created_at: row['created_at'], updated_at: row['updated_at'], name: row['name'], parent_id: row['parent_id'])
+  # end
 
-  CSV.foreach('db/categories.csv', headers: true) do |row|
-    Category.create(id: row['id'], created_at: row['created_at'], updated_at: row['updated_at'], name: row['name'], parent_id: row['parent_id'])
-  end
-
-  CSV.foreach('db/brands.csv', headers: true) do |row|
-    Brand.create(id: row['id'], created_at: row['created_at'], updated_at: row['updated_at'], name: row['name'], parent_id: row['parent_id'])
-  end
+  # CSV.foreach('db/brands.csv', headers: true) do |row|
+  #   Brand.create(id: row['id'], created_at: row['created_at'], updated_at: row['updated_at'], name: row['name'], parent_id: row['parent_id'])
+  # end
 end
