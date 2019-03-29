839ed14f71c91849dd10400b3603fedbfadbd912
umigit <hamu56Kiki>
2019-03-29 15:26:58 +0900

    recaptcha discription

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations", sessions: "users/sessions"}
   root "items#index"
   resources :items, only: [:new, :create] do
     member do
