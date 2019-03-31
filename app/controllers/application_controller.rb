c1fdc0595986d1762ac40cfd4a8b9d97034d6c25
fqllen <kohei199875@icloud.com>
2019-03-31 21:10:56 +0900

    fields_forの記載

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_paramaters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_paramaters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [user_profile_attributes: [:user_id, :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_y, :birth_m, :birth_d]])
+  end
 end
