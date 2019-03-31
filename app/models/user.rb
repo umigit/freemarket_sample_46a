1fa2bded90981b208ad79413c3abce8a36fdc692
fqllen <kohei199875@icloud.com>
2019-03-31 21:12:15 +0900

    アソシエーションfields_for

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
   validates :email,                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'のフォーマットが不適切です' }
   has_many :sns_credentials
 
   has_one :user_profile, dependent: :destroy
   accepts_nested_attributes_for :user_profile, update_only: true
+
 
   def self.from_omniauth(auth)
     credential = SnsCredential.where(provider: auth.provider, uid: auth.uid).first
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,5 @@
 class UserProfile < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
+
+  belongs_to :user, inverse_of: :user_profile, optional: true
 end
