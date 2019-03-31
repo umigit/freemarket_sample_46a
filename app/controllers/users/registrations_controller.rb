7dfcd8d27c07ba9613bc94d182be76b13520aa0a
fqllen <kohei199875@icloud.com>
2019-03-31 21:10:41 +0900

    新規登録コントローラ作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,86 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+before_action :configure_sign_up_params, only: [:create]
+
   def sns
     @user = User.new(
       email: session[:email],
       password: session[:password],
-      password_confirmation: session[:password],
+      password_confirmation: session[:password]
+      )
+    @user.build_user_profile(
+      nickname: session[:nickname],
+      last_name: session[:last_name],
+      first_name: session[:first_name],
+      last_name_kana: session[:last_name_kana],
+      first_name_kana: session[:first_name_kana],
+      birth_y: session[:birth_y],
+      birth_m: session[:birth_m],
+      birth_d: session[:birth_d]
       )
   end
 
-  def create
+
+
+
+  def phone
   end
 
+
+
+
+
+  def region
+  end
+
+
+
+
+  def credit
+  end
+
+
+
+
+  def complete
+  end
+
+
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'sns'
+    end
+  end
+
+
+
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up) do |params|
+      params.permit(:email, :password, :password_confirmation,
+                    user_profile_attributes: [:user_id, :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_y, :birth_m, :birth_d])
+
+    end
+  end
 end
-  # before_action :configure_sign_up_params, only: [:create]
+
+
+
+
+
+
+
+
+
+
+
+
+
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
