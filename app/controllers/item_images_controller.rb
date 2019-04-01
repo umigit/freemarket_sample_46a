add072e08197b970945814ddd421ba9d38b94e3d
umigit <hamu56Kiki>
2019-04-01 17:11:59 +0900

    add update action to item_images_controller

--- app/controllers/item_images_controller.rb
+++ app/controllers/item_images_controller.rb
@@ -1,7 +1,17 @@
 class ItemImagesController < ApplicationController
+  def update
+    item_image = Itemimage.find(params[:id])
+    item_image.update(item_image_params)
+  end
   def destroy
     item_image = ItemImage.find(params[:id])
     item_image.destroy
+  end
+
+  private
+
+  def item_image_params
+    params.require(:item_image).permit(:image)
   end
 end
 
