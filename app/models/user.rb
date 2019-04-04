6c2ed02170e97d09d878b495b855003d5ad3d6d5
fqllen <kohei199875@icloud.com>
2019-04-05 03:37:54 +0900

    fields_forのための記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,10 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
   validates :email,                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'のフォーマットが不適切です' }
   has_many :sns_credentials
-  has_one :user_profile
-
   has_one :user_profile, dependent: :destroy
+  has_one :address, dependent: :destroy
   accepts_nested_attributes_for :user_profile, update_only: true
-
+  accepts_nested_attributes_for :address, update_only: true
 
   def self.from_omniauth(auth)
     credential = SnsCredential.where(provider: auth.provider, uid: auth.uid).first
